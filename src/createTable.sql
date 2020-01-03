ed34feb956f7a08cd85fb127684c2252814756fa
Al Proulx <alain.proulx@hotmail.com>
2020-01-03 11:52:27 -0500

    Updated CreateTable sql statement

--- src/createTable.sql
+++ src/createTable.sql
@@ -1,30 +1,27 @@
-CREATE TABLE public.tbl_os_feedback
-(
-    yminimum double precision,
-    ymaximum double precision,
-    xminimum double precision,
-    xmaximum double precision,
-    scale double precision,
-    rating double precision,
-    for_business_use integer,
-    email text COLLATE pg_catalog."default",
-    comments text COLLATE pg_catalog."default",
-    centery double precision,
-    centerx double precision,
-    date_created date,
-    id uuid DEFAULT uuid_generate_v1(),
-    other_uses text COLLATE pg_catalog."default",
-    education boolean,
-    recreation boolean,
-    real_estate boolean,
-    business boolean,
-    delivery boolean,
-    economic_development boolean
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-ALTER TABLE public.tbl_os_feedback
-    OWNER to postgres;
+CREATE TABLE public.tbl_os_feedback (
+	yminimum float8 NULL,
+	ymaximum float8 NULL,
+	xminimum float8 NULL,
+	xmaximum float8 NULL,
+	"scale" float8 NULL,
+	rating float8 NULL,
+	for_business_use int4 NULL,
+	email text NULL,
+	"comments" text NULL,
+	centery float8 NULL,
+	centerx float8 NULL,
+	date_created date NULL,
+	id uuid NULL DEFAULT uuid_generate_v1(),
+	other_uses text NULL,
+	education bool NULL,
+	recreation bool NULL,
+	real_estate bool NULL,
+	business bool NULL,
+	delivery bool NULL,
+	economic_development bool NULL,
+	report_problem bool NULL,
+	my_maps_id varchar(50) NULL,
+	feature_id varchar(50) NULL
+);
