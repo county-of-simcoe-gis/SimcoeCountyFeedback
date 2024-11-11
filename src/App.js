8275162342dfebcb58462f4a9f041367b9576d25
Tom <58078313+reed-tom@users.noreply.github.com>
2024-11-11 09:25:46 -0500

    fixed issue with null email

--- src/App.js
+++ src/App.js
@@ -183,7 +183,7 @@ const App = () => {
 
   const validateEmail = (email) => {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    return re.test(String(email).toLowerCase()) || !email;
   };
 
   const getJSON = (url, callback) => {
